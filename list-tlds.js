0e438a8afd80c110e7ecbd7c122035d80d29c278
Hugo DARDENNE <hugo.dardenne@wanadoo.fr>
2021-11-03 11:42:21 +0100

    Updating "list TLD" with Namebase API changes

--- list-tlds.js
+++ list-tlds.js
@@ -1,11 +1,12 @@
-
 (async function () {
+  const timeDelay = 1250;
   let domains = [];
   let done = false;
+  let index = 0;
   while (!done) {
     const resp = await fetch(
       'https://www.namebase.io/api/user/domains/not-listed/' +
-        domains.length +
+        index +
         '?sortKey=acquiredAt&sortDirection=desc&limit=100',
       {
         credentials: 'include',
@@ -25,26 +26,41 @@
     if (newDomains.length === 0) {
       done = true;
     } else {
-      await new Promise(res => setTimeout(() => res(), 1000));
+      index += newDomains.length;
+      await new Promise(res => setTimeout(() => res(), timeDelay));
     }
   }
 
-  const listedResp = await fetch(
-    'https://www.namebase.io/api/user/domains/listed',
-    {
-      credentials: 'include',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      referrer: 'https://www.namebase.io/manage/listed',
-      method: 'GET',
-      mode: 'cors',
+  await new Promise(res => setTimeout(() => res(), timeDelay));
+
+  done = false;
+  index = 0;
+  while (!done) {
+    const resp = await fetch(
+      'https://www.namebase.io/api/user/domains/listed/' + index + '?limit=100',
+      {
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Cache-Control': 'max-age=0',
+        },
+        referrer: 'https://www.namebase.io/manage/owned?page=1',
+        method: 'GET',
+        mode: 'cors',
+      }
+    );
+    const json = await resp.json();
+    const newDomains = json.domains.map(a => a.name);
+    domains = domains.concat(newDomains);
+    if (newDomains.length === 0) {
+      done = true;
+    } else {
+      index += newDomains.length;
+      await new Promise(res => setTimeout(() => res(), timeDelay));
     }
-  );
-  const listedJson = await listedResp.json();
-  const listedDomains = listedJson.domains.map(a => a.name);
-  domains = domains.concat(listedDomains);
+  }
+
   const hiddenLink = document.createElement('a');
   hiddenLink.href =
     'data:text/txt;charset=utf-8,' + encodeURI(domains.join('\n'));
