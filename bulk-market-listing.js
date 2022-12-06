e7c09924a7b93f81ca74155ca2793427e4cfc1a5
HDardenne <HDardenne@users.noreply.github.com>
2022-12-06 17:55:38 +0000

    Add the possibility to have both default and specific descriptions while bulk listing

--- bulk-market-listing.js
+++ bulk-market-listing.js
@@ -1,14 +1,20 @@
 (async function () {
+  // Default description used if there isn't one set for the TLD
+  const defaultDescription = "Contact XYZ for more details"
   const tlds = [
     {
       name: 'tld-1',
       price: 1000,
-	    description: 'tld-1 is the perfect name for blah blah blah flexing'
     },
     {
       TLDName: 'tld-2',
       price: 2000,
-      description: 'tld-2 - a great handshake name blah blah blah brochure text here'
+      description: 'tld-2 - a great handshake name ...'
+    },
+    {
+      TLDName: 'tld-3',
+      price: 5000,
+      description: 'tld-3 is the perfect name for ...'
     },
   ].filter(t=>t.price);
   for (let n of tlds) {
@@ -25,7 +31,7 @@
           '{"amount":"' +
           n.price +
           '","asset":"HNS","description":"' +
-          n.description +
+          (n.description ?? defaultDescription) +
           '"}',
         method: 'POST',
         mode: 'cors',
